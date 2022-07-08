f5d6b6f6a3e8c337d41bcdb7df2e5f5428d04974
RezaNorm <rezanevernorm@tutanota.com>
2022-07-08 19:59:18 +0430

    add auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return `fuck you`;
+  }
+
+  signin() {
+    return `fuck you`;
+  }
+}
